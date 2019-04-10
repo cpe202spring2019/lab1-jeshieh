58e7aee3b184ba2abf17c5b1d3b9bee0c7b9ffa7
jeshieh <jeshieh@calpoly.edu>
2019-04-10 16:18:05 -0700

    I made these changes.

--- lab1.py
+++ lab1.py
@@ -1,15 +1,59 @@
-
 def max_list_iter(int_list):  # must use iteration not recursion
-   """finds the max of a list of numbers and returns the value (not the index)
-   If int_list is empty, returns None. If list is None, raises ValueError"""
-   pass
+    """finds the max of a list of numbers and returns the value (not the index)
+    If int_list is empty, returns None. If list is None, raises ValueError"""
+    if int_list == []:
+        return None
 
-def reverse_rec(int_list):   # must use recursion
-   """recursively reverses a list of numbers and returns the reversed list
-   If list is None, raises ValueError"""
-   pass
+    elif int_list == None:
+        raise ValueError
+
+    else:
+        max_num = int_list[0]
+        for item in int_list:
+            if item > max_num:
+                max_num = item
+
+            return max_num
+
+
+def reverse_rec(int_list):  # must use recursion
+    """recursively reverses a list of numbers and returns the reversed list
+    If list is None, raises ValueError"""
+
+    if int_list == None:
+        raise ValueError
+
+    if len(int_list) != 1:
+        return_list = reverse_rec(int_list[1:])
+        return_list.append(int_list[0])
+        return return_list
+    elif len(int_list) == 1:
+        return int_list
+
+
+
+
 
 def bin_search(target, low, high, int_list):  # must use recursion
-   """searches for target in int_list[low..high] and returns index if found
+    """searches for target in int_list[low..high] and returns index if found
    If target is not found returns None. If list is None, raises ValueError """
-   pass
+
+    if int_list == None:
+        raise ValueError
+
+    index = int((low + high) // 2)
+
+    if int_list[index] == target:
+        return index
+
+    if target < int_list[low] or target > int_list[high]:
+        return None
+
+
+    if int_list[index] > target:
+        return bin_search(target, low, index - 1, int_list)
+
+    else:
+        return bin_search(target, index + 1, high, int_list)
+
+
--- lab1_test_cases.py
+++ lab1_test_cases.py
@@ -1,4 +1,4 @@
-aimport unittest
+import unittest
 from lab1 import *
 
  # A few test cases.  Add more!!!
@@ -11,13 +11,56 @@ class TestLab1(unittest.TestCase):
             max_list_iter(tlist)
 
     def test_reverse_rec(self):
+        #Tests if it reverses a list of all positives
         self.assertEqual(reverse_rec([1,2,3]),[3,2,1])
 
+        #Tests if it reverses a list of all negatives
+        self.assertEqual(reverse_rec([-5,-1,-4,-5]), [-5,-4,-1,-5])
+
+        #Tests if it reverses a list of repeated numbers
+        self.assertEqual(reverse_rec([0, 0, 0]), [0, 0, 0])
+
+
     def test_bin_search(self):
+
+        #Tests if binary search can find a target
         list_val =[0,1,2,3,4,7,8,9,10]
         low = 0
         high = len(list_val)-1
-        self.assertEqual(bin_search(4, 0, len(list_val)-1, list_val), 4 )
+        self.assertEqual(bin_search(4, 0, len(list_val)-1, list_val), 4)
+
+        #Tests if binary search can raise a value error
+        with self.assertRaises(ValueError):
+            bin_search(4, 0, len(list_val)-1, None)
+
+        # Tests if target is not in list but whole list is searched
+        self.assertEqual(bin_search(-8, 0, len(list_val) - 1, list_val), None)
+
+        #Tests if target is not in list but only part of list is searched
+        list_val =[0,1,2,3,4,7,8,9,10]
+        low = 3
+        high = len(list_val) - 1
+        self.assertEqual(bin_search(1, low, high, list_val), None)
+
+        #Tests of target is in the middle of the list
+        list_val = [1, 2, 3, 4, 5, 6, 7]
+        low = 0
+        high = len(list_val) - 1
+        self.assertEqual(bin_search(4, low, high, list_val), 3)
+
+        #Tests if target is at beginning
+        list_val = [1, 3, 5, 11, 16, 18, 31, 60]
+        low = 0
+        high = len(list_val)-1
+        self.assertEqual(bin_search(1, low, high, list_val), 0)
+
+        #Tests if target is at end
+        list_val = [1, 3, 5, 11, 16, 18, 31, 60]
+        low = 0
+        high = len(list_val) - 1
+        self.assertEqual(bin_search(60, low, high, list_val), 7)
+
+
 
 if __name__ == "__main__":
         unittest.main()
